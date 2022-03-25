b4d1b80f0cea6dd1dfcb6efbe84a7cf93ad41ff2
Nicholas Lucien <nicklucien@gmail.com>
2022-03-24 18:29:10 -0700

    first commit

---
+++ posts/views.py
@@ -0,0 +1,36 @@
+
+from ast import Delete
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (
+    CreateView, 
+    UpdateView, 
+    DeleteView
+)
+from django.urls import reverse_lazy
+from .models import Post
+
+
+class PostListView(ListView):
+    template_name = "posts/posts_list.html"
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostCreateView(CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ['title', 'text']
+
+class PostUpdateView(UpdateView):
+    template_name = "post/edit.html"
+    model = Post
+    fields = ['title', 'text']
+
+class PostDetailView(DeleteView):
+    template_name = "post/delete.html"
+    model = Post
+    success_url = reverse_lazy('')
+
+
